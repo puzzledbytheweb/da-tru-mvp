18c00fe41f60bd6370134e8e0f0a3a9cf5d2617d
Pedro Filipe <pedro.filipe@kaluza.com>
2020-08-17 15:42:00 +0100

    Add default levels

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,42 @@ interface DaTruMvpOptions {
   githubAccessToken: string;
 }
 
+const defaultLevels: Level[] = [
+  {
+    name: 'Apprentice',
+    description:
+      'An Apprentice of the arts of the Force. True potential in the form of a strong connection to the Force.',
+    threshold: 1,
+  },
+  {
+    name: 'Padawan',
+    description: 'A fully-fledged Jedi at the beginning of their Journey.',
+    threshold: 10,
+  },
+  {
+    name: 'Knight',
+    description:
+      'An experienced Jedi capable of taking ownership of important and challenging missions.',
+    threshold: 30,
+  },
+  {
+    name: 'Master',
+    description:
+      'A true Master in the arts of the Force, capable of taking ownership of crucial missions and mentorship of lower stage Jedis.',
+    threshold: 80,
+  },
+  {
+    name: 'Legendary',
+    description:
+      'A legend taught in the records of the Jedi Archive. It is a priviledge to work with this individual, life-changing.',
+    threshold: 200,
+  },
+];
+
 export class DaTruMvp {
   levels: Level[];
   constructor(options: DaTruMvpOptions) {
-    this.levels = options.levels;
+    this.levels = options.levels || defaultLevels;
   }
 
   getLevelForGivenUser(user: GithubUser): Level | null {
