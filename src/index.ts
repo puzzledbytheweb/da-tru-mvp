d7f180b9daab9caa528c9390db2777b62de6046c
Pedro Filipe <pedro.filipe@kaluza.com>
2020-08-25 18:06:03 +0100

    Add Github plugin to class constructor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { Octokit } from '@octokit/rest';
+import { retry } from '@octokit/plugin-retry';
+import { throttling } from '@octokit/plugin-throttling';
 import { GithubUser } from './octokit';
+import { Logger } from './utils/logger';
+import prettyMs from 'pretty-ms';
 
 interface Level {
   name: string;
@@ -10,6 +15,83 @@ interface Level {
 interface DaTruMvpOptions {
   levels?: Level[];
   githubAccessToken: string;
+  baseUrl?: string;
+}
+
+interface ThrottleOpts {
+  /** The request object */
+  request: { /** What retry we are on */ retryCount: number };
+  /** API method that was throttled */
+  method: string;
+  /** URL that was throttled */
+  url: string;
+}
+
+export class DaTruMvp {
+  readonly github: Octokit;
+
+  levels: Level[];
+  baseUrl: string;
+  logger: Logger;
+
+  constructor(options: DaTruMvpOptions, logger: Logger) {
+    if (!options.githubAccessToken || options.githubAccessToken === '')
+      throw new Error('You need to define a Github Access Token');
+
+    this.levels = options.levels || defaultLevels;
+    this.baseUrl = options.baseUrl || 'https://api.github.com';
+    this.logger = logger;
+
+    // https://github.com/intuit/auto/blob/5f3e936d5caa4adaef8939e478fa49c51850a45f/packages/core/src/git.ts#L132
+    const GitHub = Octokit.plugin(retry, throttling);
+    this.github = new GitHub({
+      baseUrl: this.baseUrl,
+      auth: options.githubAccessToken,
+      previews: ['symmetra-preview'],
+      throttle: {
+        /** Add a wait once rate limit is hit */
+        onRateLimit: (retryAfter: number, opts: ThrottleOpts) => {
+          this.logger.log.warn(
+            `Request quota exhausted for request ${opts.method} ${opts.url}`
+          );
+
+          if (opts.request.retryCount < 5) {
+            this.logger.log.log(
+              `Retrying after ${prettyMs(retryAfter * 1000)}!`
+            );
+            return true;
+          }
+
+          return false;
+        },
+        /** wait after abuse */
+        onAbuseLimit: (retryAfter: number, opts: ThrottleOpts) => {
+          this.logger.log.error(
+            `Went over abuse rate limit ${opts.method} ${
+              opts.url
+            }, retrying in ${prettyMs(retryAfter * 1000)}.`
+          );
+          return true;
+        },
+      },
+    });
+  }
+
+  getLevelForGivenUser(user: GithubUser): Level | null {
+    if (user.type !== 'User') return null;
+
+    const foundLevel = this.levels.find(
+      level => level.threshold >= user.contributions
+    );
+
+    // Get the highest threshold level
+    if (!foundLevel)
+      return this.levels.sort(
+        (levelA, levelB) => levelB.threshold - levelA.threshold
+      )[0];
+
+    return foundLevel;
+  }
 }
 
 const defaultLevels: Level[] = [
@@ -43,29 +125,3 @@ const defaultLevels: Level[] = [
     threshold: 200,
   },
 ];
-
-export class DaTruMvp {
-  levels: Level[];
-  constructor(options: DaTruMvpOptions) {
-    if (!options.githubAccessToken || options.githubAccessToken === '')
-      throw new Error('You need to define a Github Access Token');
-
-    this.levels = options.levels || defaultLevels;
-  }
-
-  getLevelForGivenUser(user: GithubUser): Level | null {
-    if (user.type !== 'User') return null;
-
-    const foundLevel = this.levels.find(
-      level => level.threshold >= user.contributions
-    );
-
-    // Get the highest threshold level
-    if (!foundLevel)
-      return this.levels.sort(
-        (levelA, levelB) => levelB.threshold - levelA.threshold
-      )[0];
-
-    return foundLevel;
-  }
-}
---
+++ src/utils/logger.ts
@@ -0,0 +1,64 @@
+import signale from 'signale';
+
+export type LogLevel = undefined | 'verbose' | 'veryVerbose' | 'quiet';
+
+export interface Logger {
+  /** The level at which to log messages */
+  logLevel?: LogLevel;
+  /** The default logger. Always on */
+  log: signale.Signale<signale.DefaultMethods>;
+  /** The verbose log. Has more debug logs */
+  verbose: signale.Signale<signale.DefaultMethods>;
+  /** The very verbose log. Has all debug logs */
+  veryVerbose: signale.Signale<signale.DefaultMethods>;
+}
+
+/** Create a dummy logger for testing. */
+export function dummyLog(): Logger {
+  return {
+    log: new signale.Signale({ disabled: true }),
+    verbose: new signale.Signale({ disabled: true }),
+    veryVerbose: new signale.Signale({ disabled: true }),
+  };
+}
+
+const logger: Logger = {
+  log: new signale.Signale(),
+  verbose: new signale.Signale(),
+  veryVerbose: new signale.Signale(),
+};
+
+/** Turn the logs on an off */
+function toggleLogs(
+  options: Record<Exclude<keyof Logger, 'logLevel'>, boolean>
+) {
+  Object.entries(options).forEach(([level, enabled]) => {
+    if (enabled) {
+      // @ts-ignore
+      logger[level].enable();
+    } else {
+      // @ts-ignore
+      logger[level].disable();
+    }
+  });
+}
+
+/** Set the log level */
+export function setLogLevel(newLogLevel: LogLevel) {
+  logger.logLevel = newLogLevel;
+
+  if (logger.logLevel === 'verbose') {
+    toggleLogs({ log: true, verbose: true, veryVerbose: false });
+  } else if (logger.logLevel === 'veryVerbose') {
+    toggleLogs({ log: true, verbose: true, veryVerbose: true });
+  } else if (logger.logLevel === 'quiet') {
+    toggleLogs({ log: false, verbose: false, veryVerbose: false });
+  } else {
+    toggleLogs({ log: true, verbose: false, veryVerbose: false });
+  }
+}
+
+/** Create a logger the the given log level. */
+export default function createLog(): Logger {
+  return logger;
+}
